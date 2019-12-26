2ffe6932808968914a8c850d07563639edb5e96a
Luis de Leon <luyzdeleon@gmail.com>
2019-12-26 12:22:58 -0400

    Refactored AAT class to have a simpler interface

--- tests/aat.spec.ts
+++ tests/aat.spec.ts
@@ -9,56 +9,44 @@ const applicationPublicKey = '0x80Aa10e5d840db15dDFD3e0B46C63EE9a567780B';
 const applicationPrivateKey = 'E73BE2AD96721D350CCFAEBF30CB2A4160652940588987EF56A9DD0FAE8042CB';
 
 describe('PocketAAT Class tests', () => {
-  it('should instantiate a PocketAAT instance', () => {
-    var pocketAAT = new PocketAAT(version, clientPublicKey, applicationPublicKey);
+  context('Valid AAT parameters', () => {
+    it('should instantiate a valid PocketAAT instance', () => {
+      var pocketAAT = new PocketAAT(clientPublicKey, applicationPublicKey, applicationPrivateKey);
+      expect(pocketAAT).to.not.be.an.instanceof(Error);
+      expect(pocketAAT).to.be.an.instanceof(PocketAAT);
+      expect(pocketAAT.isValid()).to.equal(true);
+    }).timeout(0);
 
-    expect(pocketAAT).to.not.be.an.instanceof(Error);
-    expect(pocketAAT).to.be.an.instanceof(PocketAAT);
-  }).timeout(0);
+    it('should have version 0.0.1', () => {
+      var pocketAAT = new PocketAAT(clientPublicKey, applicationPublicKey, applicationPrivateKey);
+      expect(pocketAAT).to.not.be.an.instanceof(Error);
+      expect(pocketAAT).to.be.an.instanceof(PocketAAT);
+      expect(pocketAAT.isValid()).to.equal(true);
+      expect(pocketAAT.version).to.equal('0.0.1');
+    }).timeout(0);
+  });
 
-  it('should fail to instantiate a PocketAAT instance', () => {
-    var foo = function() {
-      return new PocketAAT(version, clientPublicKey, '');
-    };
-    expect(foo).to.throw(TypeError);
-  }).timeout(0);
+  context('Invalid AAT parameters', () => {
+    it('should fail given an empty clientPublicKey', () => {
+      var foo = function() {
+        return new PocketAAT('', applicationPublicKey, applicationPrivateKey);
+      };
+      expect(foo).to.throw(TypeError);
+    }).timeout(0);
 
-  it('AATMessage should be valid', () => {
-    var pocketAAT = new PocketAAT(version, clientPublicKey, applicationPublicKey);
+    it('should fail given an empty applicationPublicKey', () => {
+      var foo = function() {
+        return new PocketAAT(clientPublicKey, '', applicationPrivateKey);
+      };
+      expect(foo).to.throw(TypeError);
+    }).timeout(0);
 
-    expect(pocketAAT.isValid()).to.equal(true);
-  }).timeout(0);
-
-  it('Should successfully create a hash', () => {
-    var pocketAAT = new PocketAAT(version, clientPublicKey, applicationPublicKey);
-
-    var hash = pocketAAT.hash();
-
-    expect(hash).to.not.be.empty;
-  }).timeout(0);
-
-  it('Should sign the token using the applicationPrivateKey', () => {
-    var pocketAAT = new PocketAAT(version, clientPublicKey, applicationPublicKey);
-
-    try {
-      pocketAAT.sign(applicationPrivateKey);
-
-      expect(pocketAAT.signature).to.not.be.empty;
-    } catch (error) {
-      expect(error).to.be.empty;
-    }
-  }).timeout(0);
-
-  it('Should fail to sign the token using an empty applicationPrivateKey', () => {
-    var pocketAAT = new PocketAAT(version, clientPublicKey, applicationPublicKey);
-
-    try {
-      pocketAAT.sign('');
-
-      expect(pocketAAT).to.not.be.an.instanceof(PocketAAT);
-    } catch (error) {
-      expect(error).to.be.an.instanceof(Error);
-    }
-  }).timeout(0);
+    it('Should fail to sign the token using an empty applicationPrivateKey', () => {
+      try {
+        new PocketAAT(clientPublicKey, applicationPublicKey, '');
+      } catch (error) {
+        expect(error).to.be.an.instanceof(Error);
+      }
+    }).timeout(0);
+  });
 });
-
