687cca0ff6748171bfd10607494e75b7f95f5a23
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-21 17:44:04 -0400

    added the enums and helper utils

---
+++ src/utils/enums.ts
@@ -0,0 +1,27 @@
+/**
+*
+* Versions enum
+*/
+export enum Versions {
+    "0.0.1" = "0.0.1"
+}
+
+export namespace Versions {
+    /**
+    *
+    * Checks if the provided version is supported
+    * @param {string} str - Provided version string.
+    * @returns {boolean} - If is supported or not.
+    * @memberof Versions
+    */
+    export function isSupported(version: string): boolean {
+        switch (version) {
+            case Versions["0.0.1"].toString():
+                return true
+            case "":
+                return false
+            default:
+                return false
+        }
+    }
+}
